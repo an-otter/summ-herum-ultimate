2e5793e06ede25563114234e862a007f4ec5b170
bienchen <heike.ademmer@th-brandenburg.de>
2025-12-12 16:24:52 +0100

    commit cache santa

--- src/main/java/bienchen/summherum/SummHerumApplication.java
+++ src/main/java/bienchen/summherum/SummHerumApplication.java
@@ -2,8 +2,10 @@ package bienchen.summherum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching; // <- wichtiger Import
 
 @SpringBootApplication
+@EnableCaching // <- Das schaltet den Cache-Mechanismus grundlegend an
 public class SummHerumApplication {
 
 	public static void main(String[] args) {
